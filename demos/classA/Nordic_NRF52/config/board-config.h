bfac5172b72ecdab024af32199316e2b5e405bd9
Ravishankar Bhagavandas <bhagavar@amazon.com>
2020-10-26 11:20:45 -0700

    Adding third party licenses

--- demos/classA/Nordic_NRF52/config/board-config.h
+++ demos/classA/Nordic_NRF52/config/board-config.h
@@ -3,7 +3,31 @@
  *
  * \brief     Board configuration
  *
- * \copyright Revised BSD License, see section \ref LICENSE.
+ * Revised BSD License
+ * Copyright Semtech Corporation 2013. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the Semtech corporation nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL SEMTECH CORPORATION BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  *
  * \code
  *                ______                              _
@@ -44,12 +68,11 @@ extern "C"
 /**
  * @brief EUI and keys that needs to be provisioned for device identity and security.
  */
- 
 #error "Please configure DEV EUI, Join EUI and App NWK key to run the demo"
 
-#define DEV_EUI       {}
-#define JOIN_EUI      {}
-#define APP_NWK_KEY   {}
+#define DEV_EUI       {};
+#define JOIN_EUI      {};
+#define APP_NWK_KEY   {};
 
 
 #define BOARD_TCXO_WAKEUP_TIME                      0
